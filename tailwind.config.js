1b3852af9178dd4f146a1a38d681a8f1578908c5
mohammadhassanbaloch <hassan.khan22145@gmail.com>
2023-05-04 21:28:20 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#008000",
+        secondary: "#968888",
+        tertiary: "#151515",
+      }
+    },
+  },
+  plugins: [],
+}
+
